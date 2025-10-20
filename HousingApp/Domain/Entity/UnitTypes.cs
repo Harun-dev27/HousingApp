c1f7383a8c27fa11bae02eea58146cddc4120c13
Harun <harunabdirahman5@gmail.com>
2025-10-21 00:00:54 +0300

    Removed Rent property

--- HousingApp/Domain/Entity/UnitTypes.cs
+++ HousingApp/Domain/Entity/UnitTypes.cs
@@ -5,11 +5,9 @@ public class UnitTypes
 {
     public string UnitTypeId { get; set; }
     public string UnitTypeName { get; set; }
-    public decimal Rent { get; set; }
     public UnitTypes(string unitTypeId, string unitTypeName, decimal Rent)
     {
         UnitTypeId = unitTypeId;
         UnitTypeName = unitTypeName;
-        this.Rent = Rent;
     }
 }
