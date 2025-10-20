1833c43b54b07e4754833f345688705cdaef6904
Harun <harunabdirahman5@gmail.com>
2025-10-20 22:20:54 +0300

    Addition of  new Property

--- HousingApp/Domain/Entity/Apartment.cs
+++ HousingApp/Domain/Entity/Apartment.cs
@@ -8,19 +8,21 @@ public class Apartment
     public string Street { get; set; }
     public string Location { get; set; }
     public int NumberOfUnits { get; set; }
+    public boolean IsVacant { get; set; }
     public Boolean ParkingAvailable { get; set; }
     public Boolean WaterAvailable { get; set; }
     public Boolean WifiAvailable { get; set; }
     public Boolean PetsAllowed { get; set; }
 
 
-    public Apartment(string apartmentName, string address, string street, string location, int numberOfUnits, Boolean parkingAvailable, Boolean waterAvailable, Boolean wifiAvailable, Boolean petsAllowed)
+    public Apartment(string apartmentName, string address, string street, string location, int numberOfUnits, Boolean isVacant, Boolean parkingAvailable, Boolean waterAvailable, Boolean wifiAvailable, Boolean petsAllowed)
     {
         ApartmentName = apartmentName;
         Address = address;
         Street = street;
         Location = location;
         NumberOfUnits = numberOfUnits;
+        IsVacant = isVacant;
         ParkingAvailable = parkingAvailable;
         WaterAvailable = waterAvailable;
         WifiAvailable = wifiAvailable;
